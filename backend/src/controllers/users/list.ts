79f5e803ae82d51d6c021dce5a509721c64e1ab4
lanaschuster <lanaschuster1@gmail.com>
2020-09-19 16:14:28 -0300

    fix list user

--- backend/src/controllers/users/list.ts
+++ backend/src/controllers/users/list.ts
@@ -1,10 +1,48 @@
-import { getConnection } from 'typeorm'
+import { getConnection, Like } from 'typeorm'
 import { User } from '../../entities/User'
 
 const list = async (req, res) => {
   const UserRepository = getConnection('sonia').getRepository(User)
-  const users = await UserRepository.query('SELECT * FROM administration.user');
-  return res.status(200).json(users)
+
+  let { filter, page, size, sort, direction } = req.query
+  const limit = size ? +size : 5
+  const offset = page ? page * limit : 0
+  sort = sort ? sort : 'name'
+  direction = direction ? direction : 'ASC'
+  let condition = undefined
+
+  if (filter && filter !== ''){
+    condition = [
+      {name: Like(`%${filter}%`)}, 
+      {username: Like(`%${filter}%`)},
+      {email: Like(`%${filter}%`)}
+    ]
+  }
+
+  const [users, total] = await UserRepository.findAndCount({
+    where: condition,
+    take: limit,
+    skip: offset,
+    order: {
+      name: direction
+    }
+  })
+
+  users.forEach(user => {
+    user.password = undefined
+    user.passwordResetToken = undefined
+    user.passwordResetExpires = undefined
+  })
+
+  const currentPage = page ? +page : 0;
+  const totalPages = Math.ceil(total / limit);
+
+  return res.status(200).json({
+    currentPage,
+    totalPages,
+    total,
+    content: users
+  })
 }
 
 export { list }
