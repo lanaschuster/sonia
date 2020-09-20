3d5b07e2e934222b39a231854ce78102f09a2acd
lanaschuster <lanaschuster1@gmail.com>
2020-09-20 12:27:06 -0300

    ref worker

---
+++ backend/src/controllers/workers/findByUser.ts
@@ -0,0 +1,38 @@
+import { getConnection } from 'typeorm'
+import { Worker } from '../../entities/Worker'
+import { User } from '../../entities/User'
+import { Department } from '../../entities/Department'
+
+const findByUser = async (req, res) => {
+  const WorkerRepository = getConnection('sonia').getRepository(Worker)
+  const results = await WorkerRepository.query(`select * from registration.worker w where w.\"userId\" = ${req.params.id};`)
+  
+  if (results.length <= 0) {
+    return res.status(404).json({
+      messagem: `O usuário id ${req.params.id} não é membro de nenhum departamento.`
+    })
+  }
+  
+  let workers = []
+  for (let i = 0; i < results.length; ++i) {
+    workers.push(await populateWorker(results[i]))
+  }
+
+  return res.status(200).json(workers)
+}
+
+async function populateWorker(worker) {
+  const WorkerRepository = getConnection('sonia').getRepository(Worker)
+  const UserRepository = getConnection('sonia').getRepository(User)
+  const DepartmentRepository = getConnection('sonia').getRepository(Department)
+
+  let user = await UserRepository.findOne({ id: worker.userId })
+  user.password = undefined
+  user.passwordResetToken = undefined
+  user.passwordResetExpires = undefined
+
+  let department = await DepartmentRepository.findOne({ id: worker.departmentId })
+  return WorkerRepository.create({ user, department, isManager: worker.isManager, isSubstitute: worker.isSubstitute, id: worker.id })
+}
+
+export { findByUser }
--- backend/src/controllers/workers/index.ts
+++ backend/src/controllers/workers/index.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express'
 import { findByDepartment } from './findByDepartment'
+import { findByUser } from './findByUser'
 // import { create } from './create'
 // import { remove } from './remove'
 const workerRouter = Router()
 
 workerRouter.get('/:id/department', findByDepartment)
-// workerRouter.get('/:id/user', WorkerRepository.findByUserId)
+workerRouter.get('/:id/user', findByUser)
 // workerRouter.get('/:id', WorkerRepository.findById)
 // workerRouter.post('/', WorkerController.save)
 // workerRouter.delete('/:id', WorkerController.delete)
